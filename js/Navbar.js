b7061fffcecbeed3ac9dae02248077caa55aae7c
Mohamed Ahmed <ma07041705@gmail.com>
2024-03-08 21:09:58 +0300

    edited  navbar

--- js/Navbar.js
+++ js/Navbar.js
@@ -3,8 +3,9 @@ document.getElementById("menu-button").addEventListener("click", () =>
     document.getElementById("nav-list").classList.toggle("show");
 });
 
-
-
+document.getElementById("close-icon").addEventListener("click", () => {
+    document.getElementById("nav-list").classList.remove("show");
+});
 window.addEventListener("scroll", function() {
     var navbar = document.querySelector(".navbar");
     if (window.pageYOffset > 0) {
