b0da78aaaf7c8b743a04dd4e280254863a6c551d
Mohamed Ahmed <ma07041705@gmail.com>
2024-04-10 23:53:45 +0300

    contact form and API

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,11 @@
 // API
 const projectUrl = "https://my-personal-website-vlip.onrender.com/projects";
-
+const contactUrl = "https://my-personal-website-vlip.onrender.com/contacts";
 
 // DOM elements
 let projectContainer = document.getElementById("project-section")
 const formContainer = document.getElementById("propertyHouseForm" );
-
+const contactForm = document.getElementById("submitForm" );
 
 // fetch all houses
 const fetchData = () => {
@@ -52,6 +52,8 @@ paintHouses();
 
 
 
+
+// hidding and displaying the project form
 let addProjectForm = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector(".projectButton");
@@ -67,6 +69,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+// project submission 
 formContainer.addEventListener("submit", async (e) => {
     e.preventDefault();
     let imageErrorMessage = document.getElementById("imageError");
@@ -112,5 +115,74 @@ formContainer.addEventListener("submit", async (e) => {
     } catch (error) {
       console.error('Error adding project:', error);
     }
-  });
+});
   
+
+
+// submitting cont info for users
+contactForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  let nameErrorMessage = document.getElementById("nameError");
+  let emailErrorMessage = document.getElementById("emailError"); 
+  let subjectErrorMessage = document.getElementById("subjectError"); 
+  let messageErrorMessage = document.getElementById("messageError"); 
+  let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  
+
+    // Reset error messages
+    emailErrorMessage.innerText = "";
+    nameErrorMessage.innerText = "";
+    subjectErrorMessage.innerText = "";
+    messageErrorMessage.innerText = "";
+
+    // Retrieve  values
+    let email = document.getElementById("email").value;
+    let name = document.getElementById("name").value;
+    let subject = document.getElementById("subject").value;
+    let message = document.getElementById("message").value;
+    
+    // Check if any field is empty
+    if (email === "" || name === "" || subject === "" || message === "") {
+        emailErrorMessage.innerText = "Email is required";
+        nameErrorMessage.innerText = "Name is required";    
+        subjectErrorMessage.innerText = "Subject is required";    
+        messageErrorMessage.innerText = "Message is required";    
+        return; // Exit function if any field is empty
+    }
+
+    // Validate email format
+    if (!emailRegex.test(email)) {
+        emailErrorMessage.innerText = "Invalid email format";
+        return; // Exit function if email format is invalid
+    }
+
+
+
+  let id = Math.floor(Math.random() * 100000);
+
+  const data = {
+    id: id, 
+    name: name,
+    email: email,
+    subject: subject,
+    message: message
+  };
+
+  try {
+    const response = await fetch(contactUrl, {
+      method: "POST",
+      headers: {
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to add contact');
+    }
+    console.log('House added successfully');
+
+  } catch (error) {
+    console.error('Error adding contact:', error);
+  }
+});
