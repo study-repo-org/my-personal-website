74524c86b041805ab1c0ae195a7f35561761d810
Mohamed Ahmed <ma07041705@gmail.com>
2024-04-10 23:37:08 +0300

    added post form and API

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 // API
-const projectUrl = "https://my-personal-website-vlip.onrender.com/projects";
+const projectUrl = " http://localhost:3000/projects";
 
 
 // DOM elements
@@ -43,10 +43,63 @@ async function paintHouses() {
     try {
         const projects = await fetchData();
         let cards = projects.map(project => createHouseCard(project)).join(" ");
-        console.log(projects);
         projectContainer.innerHTML = cards;
     } catch (error) {
         console.error("Error fetching project:", error.message);
     }
 }
 paintHouses();
+
+
+
+let addProjectForm = false;
+document.addEventListener("DOMContentLoaded", () => {
+  const addBtn = document.querySelector(".projectButton");
+  const projectFormContainer = document.querySelector(".project-form-container");
+  addBtn.addEventListener("click", () => {
+    addProjectForm = !addProjectForm;
+    if (addProjectForm) {
+      projectFormContainer.style.display = "block";
+    } else {
+      projectFormContainer.style.display = "none";
+    }
+  });
+});
+
+
+formContainer.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    
+    let image = document.getElementById("image").value;
+    let description = document.getElementById("description").value;
+    let title = document.getElementById("title").value;
+
+  
+    let id = Math.floor(Math.random() * 100000);
+  
+    const data = {
+      id: id, 
+      image: image,
+      title: title,
+      description: description,
+    };
+  
+    try {
+      const response = await fetch(projectUrl, {
+        method: "POST",
+        headers: {
+          "content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+  
+      if (!response.ok) {
+        throw new Error('Failed to add project');
+      }
+      console.log('House added successfully');
+  
+    } catch (error) {
+      console.error('Error adding project:', error);
+    }
+  });
+  
